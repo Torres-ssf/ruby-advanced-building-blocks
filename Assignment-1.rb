6c68d21fb912534eea0adac32ec28d40237b13bb
TorresChief <sergio.uft@gmail.com>
2019-04-29 11:44:39 -0300

    Finish bubble_sort method

---
+++ Assignment-1.rb
@@ -0,0 +1,14 @@
+test_array = [8, 5, 8, 2, 9, 34, 1, 7, 987, 32, 1, 6, 99, 10]
+
+def bubble_sort(array)
+  finish_sorted = false
+  until finish_sorted
+    finish_sorted = true
+    for i in 0...array.length - 1
+      next if(array[i] <= array[i + 1])
+      array[i], array[i + 1] = array[i + 1], array[i]
+      finish_sorted = false
+    end
+  end
+  array
+end
