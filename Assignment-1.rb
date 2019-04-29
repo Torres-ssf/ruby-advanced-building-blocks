4635fa0b866bb4c7ddacb89b59d2d23a3a5d348e
TorresChief <sergio.uft@gmail.com>
2019-04-29 11:55:31 -0300

    Finish 'bubble_sort_by' method. Complete assignment-1

--- Assignment-1.rb
+++ Assignment-1.rb
@@ -12,3 +12,16 @@ def bubble_sort(array)
   end
   array
 end
+
+def bubble_sort_by(array)
+  finish_sorted = false
+  until finish_sorted
+    finish_sorted = true
+    for i in 0...array.length - 1
+      next unless yield(array[i], array[i + 1]) > 0
+      array[i], array[i + 1] = array[i + 1], array[i]
+      finish_sorted = false
+    end
+  end
+  array
+end
