c011de4dc99414700ac25aba5352e6a5dffe4b48
Torres-ssf <sergio.uft@gmail.com>
2019-05-11 18:32:34 -0300

    Fixed minor issues. Use 'it' instead 'context' to describe the context since 'it' makes more sense to this project. Create variable most used in the examples

--- spec/assignment_2_spec.rb
+++ spec/assignment_2_spec.rb
@@ -2,85 +2,87 @@
 load './Assignment-2.rb'
 
 RSpec.describe Enumerable do
+  let(:test_array_1) { [1, 4, 8, 9, 11] }
+  let(:test_array_2) { [1, 2, 3, 4, 5] }
   describe "#my_any?" do
-    context "verify if at least one element is higher than 10." do
-      it { expect([1, 4, 8, 9, 11].my_any? {|i| i > 10 }).to eql(true) }
+    it "verify if at least one element is higher than 10." do
+       expect(test_array_1.my_any? {|i| i > 10 }).to eql(true)
     end
 
-    context "verify if at least one element is an integer." do
-      it { expect([nil,"ruby", true, 'a', 11].my_any?(Integer)).to eql(true) }
+    it "verify if at least one element is an integer." do
+      expect([nil,"ruby", true, 'a', 11].my_any?(Integer)).to eql(true)
     end
 
-    context "verify if any element is neither false nor nil" do
-      it { expect([nil, false].my_any?).to eql(false) }
+    it "verify if any element is neither false nor nil" do
+      expect([nil, false].my_any?).to eql(false)
     end
   end
 
   describe "#my_all?" do
-    context "check if every element is higher than 5" do
-      it { expect([1, 4, 8, 9, 11].my_all? {|i| i > 5}).to eql(false) }
+    it "check if every element is higher than 5" do
+      expect(test_array_1.my_all? {|i| i > 5}).to eql(false)
     end
 
-    context "check if every element inside the array is a String" do
-      it { expect(["a", "aa", "aaa"].my_all?(String)).to eql(true) }
+    it "check if every element inside the array is a String" do
+      expect(["a", "aa", "aaa"].my_all?(String)).to eql(true)
     end
 
-    context "verify if any element never returns nil or false" do
-      it { expect([].my_all?).to eql(true) }
+    it "verify if any element never returns nil or false" do
+      expect([].my_all?).to eql(true)
     end
 
-    context "check if every String has 's' in it's content" do
-      it { expect(["cats", "dogs", "pigs", "birds"].my_all?(/s/)).to eql(true) }
+    it "check if every String has 's' in it's content" do
+      expect(["cats", "dogs", "pigs", "birds"].my_all?(/s/)).to eql(true)
     end
   end
 
   describe "#my_select" do
-    context "returns an array containing elements that satisfy the condition" do
-       it { expect([1, 4, 8, 9, 11].select {|i| i > 10} ).to eql([11]) }
+    it "returns an array containing elements that satisfy the condition" do
+      expect(test_array_1.select {|i| i > 10} ).to eql([11])
     end
   end
 
   describe "#my_none?" do
-    context "Verify if none of the elements are an integer" do
-      it { expect([2.2, "number", true].my_none?(Integer)).to eql(true) }
+    it "Verify if none of the elements are an integer" do
+      expect([2.2, "number", true].my_none?(Integer)).to eql(true)
     end
 
-    context "check if none of the elements returns true" do
-      it { expect([nil].my_none?).to eql(true) }
+    it "check if none of the elements returns true" do
+      expect([nil].my_none?).to eql(true)
     end
 
-    context "check if none of the elements returns true" do
-      it { expect([nil, false, true].my_none?).to eql(false) }
+    it "check if none of the elements returns true" do
+      expect([nil, false, true].my_none?).to eql(false)
     end
   end
 
   describe "#my_count" do
-    context "return the number of elements in the array" do
-      it { expect([1, 2, 3, 4, 5].my_count).to eql(5) }
+    it "return the number of elements in the array" do
+      expect(test_array_2.my_count).to eql(5)
     end
 
-    context "return the number of elements that are higher than 2" do
-      it { expect([1, 2, 3, 4, 5].my_count { |i| i > 2}).to eql(3) }
+    it "return the number of elements that are higher than 2" do
+      expect(test_array_2.my_count { |i| i > 2}).to eql(3)
     end
   end
 
   describe "#my_map" do
-    context "Multiply every single element by 2" do
-      it { expect([2, 4, 6, 8, 10].my_map { |i| i * 2}).to eql([4, 8, 12, 16, 20]) }
+    it "Multiply every single element by 2" do
+      expect([2, 4, 6, 8, 10].my_map { |i| i * 2}).to eql([4, 8, 12, 16, 20])
     end
   end
 
   describe "#my_inject" do
-    context "return the multiplication of every single one element." do
-      it { expect([1, 2, 3, 4, 5].my_inject { |i, y| i * y}).to eql(120) }
+    it "return the multiplication of every single one element." do
+      expect(test_array_2.my_inject { |i, y| i * y}).to eql(120)
     end
 
-    context "return the sum of every single one element." do
-      it { expect([1, 2, 3, 4, 5].my_inject { |i, y| i + y}).to eql(15) }
+    it "return the sum of every single one element." do
+      expect(test_array_2.my_inject { |i, y| i + y}).to eql(15)
     end
 
-    context "return the division of every single one element." do
-      it { expect([10000, 100, 10].my_inject { |i, y| i / y}).to eql(10) }
+    it "return the division of every single one element." do
+      expect([10000, 100, 10].my_inject { |i, y| i / y}).to eql(10)
     end
   end
 end
